0a79b40b7e0413b045b3c1321821a09d6e2dc1d9
Paul Glass-Steel <glasteel@email.unc.edu>
2015-12-10 11:28:05 -0500

    revise

--- AuthUserFromSSO.php
+++ AuthUserFromSSO.php
@@ -3,90 +3,46 @@ namespace glasteel;
 
 class AuthUserFromSSO
 {
-    protected $authorized = null;
-    protected $user;
-    protected $settings;
+    protected $auth_user = null;
 
-    public function __construct($settings = []){
-        //are these settings needed?
-        if( is_array($settings) ){
-            $this->settings = $settings;
+    public function user($user_obj=null){
+        //getter
+        if ( $user_obj === null ){
+            return $this->auth_user;
         }
-    }
+        //setter
+        $this->auth_user = $user_obj;
+    }//user()
 
-    public function setUser(){
-        
-        if ( $this->authorized !== null ){
-            return $this->authorized;
+    public function __get($property){
+        if( is_object($this->auth_user) ){
+            return $this->auth_user->$property;
         }
-
-        switch ($_ENV['MODE']) { //do we need this switch?
-            case 'local':
-                //TODO grab PID from $_ENV
-                $user = new \stdclass;
-                $user->name = 'Paul';
-                $this->user = $user;
-                $this->authorized = true;
-                return; 
-            break;
-
-            case 'development':
-                //TODO grab PID from $_SERVER
-                # code...
-            break;
-
-            case 'production':
-                //TODO grab PID from $_SERVER
-                # code...
-            break;
-            
-            default:
-                die();//dotenv should catch this first
+        return false;
+    }//__get()
+    
+    public function __call($method,$args){
+        if( is_object($this->auth_user) ){
+            return call_user_func_array([$this->auth_user,$method],$args);
         }
-    }
+        return false;
+    }//__call()
 
-        //global $app;
-        
-        // switch ($app->settings['SLIM_MODE']){
-        //     case 'local':
-        //         $pid = 714623825; //TODO mechanism to set spoof pid
-        //         $user = \R::findOne('user','unc_pid = :unc_pid',[
-        //             'unc_pid' => $pid
-        //         ]);
-        //         break;   
+    public function getPID(){
+        $pid = ( isset($_SERVER['pid']) ) ? $_SERVER['pid'] : false;
+        return $this->validatePID($pid);
+    }//getPID()
 
-        //     //TODO development case
-
-        //     //TODO production case
-
-        //     default:
-        //         die();//TODO improve error handling
-        // }
-
-        // $user = new stdclass;
-        // $user->name = 'Paul';
-        // $this->auth = $user;
-        // return true;
-
-        // if ( $user && $user->id ){
-        //     try {
-        //         $this->auth = new User($user);
-        //     } catch (Exception $e) {
-        //         pre_r($e->getMessage());   
-        //     }
-        //     return true;
-        // }
-        // $this->auth = false;
-        // return false;
-    //}
-
-    public function __get($key){
-        if( $this->authorized !== true ){
-            return false;
+    public function validatePID($pid){
+        if(
+            $pid                                //not falsey
+            && preg_match('/^[0-9]*$/', $pid)   //all numerals
+            && strlen($pid) == 9 &&             //9 digits
+            $pid[0] == '7'                      //starts with 7
+        ){
+            return $pid;
         }
-        if( is_object($this->user) && property_exists($this->user, $key) ){
-            return $this->user->$key;
-        }
-    }
+        return false;
+    }//validatePID()
 
 }//class AuthUserFromSSO
